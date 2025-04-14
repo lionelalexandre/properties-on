07339718c98fdf4cd7421671b44601455baf19a0
tprtenjak <tprtenjak@gmail.com>
2025-04-14 23:44:38 +0200

    test file

---
+++ pyscf/prop/nmr/test/test_rhf_dm_msc.py
@@ -0,0 +1,73 @@
+import unittest
+from pyscf import gto, scf
+from pyscf.prop.nmr.rhf_dm import NMR
+from pyscf.data import nist
+nist.ALPHA = 1./137.03599967994
+
+mol = gto.Mole()
+mol.verbose = 20
+mol.output = '/dev/null'
+
+mol.atom = [
+    [1   , (0. , 0. , .917)],
+    ["F" , (0. , 0. , 0.)], ]
+mol.basis = {"H": 'cc_pvdz',
+             "F": 'cc_pvdz',}
+mol.build()
+
+mf = scf.RHF(mol)
+mf.conv_tol_grad = 1e-6
+mf.conv_tol = 1e-14
+mf.scf()
+
+def finger(mat):
+    return abs(mat).sum()
+
+class TestNMRShielding(unittest.TestCase):
+
+    def test_giao(self):
+        nmr = NMR(mf)
+        nmr.cphf = False
+        nmr.gauge_orig = None
+        msc = nmr.shielding()
+        self.assertAlmostEqual(finger(msc), 1488.0948832003237, 4)
+
+    def test_common_gauge(self):
+        nmr = NMR(mf)
+        nmr.cphf = False
+        nmr.gauge_orig = (1,1,1)
+        msc = nmr.shielding()
+        self.assertAlmostEqual(finger(msc), 1636.7413245248476, 4)
+
+    def test_mcw_method(self):
+        nmr = NMR(mf)
+        nmr.cphf = False
+        nmr.gauge_orig = None
+        msc = nmr.shielding(method='mcw')
+        self.assertAlmostEqual(finger(msc), 1488.0948832003241, 4)
+        
+    def test_mcw_method_common_gauge(self):
+        nmr = NMR(mf)
+        nmr.cphf = False
+        nmr.gauge_orig = (1,1,1)
+        msc = nmr.shielding(method='mcw')
+        self.assertAlmostEqual(finger(msc), 1636.741324524848, 4)
+
+    def test_sylvester_method(self):
+        nmr =  NMR(mf)
+        nmr.cphf = False
+        nmr.gauge_orig = None
+        msc = nmr.shielding(method='slv')
+        self.assertAlmostEqual(finger(msc), 1488.09488323189, 4)
+    
+    def test_sylvester_method_common_gauge(self):
+        nmr =  NMR(mf)
+        nmr.cphf = False
+        nmr.gauge_orig = (1,1,1)
+        msc = nmr.shielding(method='slv')
+        self.assertAlmostEqual(finger(msc), 1636.741324525055, 4)
+        
+
+if __name__ == "__main__":
+    print("Running RHF-DM NMR shielding tests...")
+    unittest.main()
